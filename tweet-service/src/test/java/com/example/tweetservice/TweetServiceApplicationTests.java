4143ddb674dad2013866c4389d885cff03a18d32
Josh Long <starbuxman@gmail.com>
2018-04-05 17:21:45 -0700

    reactive stream tweet service.

--- tweet-service/src/test/java/com/example/tweetservice/TweetServiceApplicationTests.java
+++ tweet-service/src/test/java/com/example/tweetservice/TweetServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.reactivestreamsredux;
+package com.example.tweetservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ReactiveStreamsReduxApplicationTests {
+public class TweetServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
